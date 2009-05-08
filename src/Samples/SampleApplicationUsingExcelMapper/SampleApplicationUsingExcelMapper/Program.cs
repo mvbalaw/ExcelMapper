27878d82ac8fb5e5cc4e220f49d704010f30429b
shashankshetty <shashank_shetty@yahoo.com>
2009-05-08 18:59:55 +0000

    Added SampleApplicationUsingExcelMapper as an example usage of ExcelMapper

---
+++ src/Samples/SampleApplicationUsingExcelMapper/SampleApplicationUsingExcelMapper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using ExcelMapper.Configuration;
+using ExcelMapper.Repository;
+
+using ExcelToDTOMapper.DTO;
+
+using Microsoft.Practices.ServiceLocation;
+
+namespace SampleApplicationUsingExcelMapper
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ExcelMapperServiceLocator.SetUp();
+            IRepository repository = ServiceLocator.Current.GetInstance<IRepository>();
+            
+            foreach (User user in repository.Get<User>("Excel\\Users.xlsx", "User"))
+            {
+                Console.WriteLine("Hello {0} {1}", user.FirstName, user.LastName);
+            }
+        }
+    }
+}
