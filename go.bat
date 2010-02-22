b63a8c40efd96c252faa160da74022d6926a4665
shashankshetty <shashank_shetty@yahoo.com>
2010-02-22 14:31:53 +0000

    Added BuildDTOsFromExcel to the release

--- go.bat
+++ go.bat
@@ -1,2 +1,2 @@
-@tools\nant\NAnt.exe -targetframework:net-3.5 -buildfile:ExcelMapper.build -D:use-svn-revision=false -D:debug=true %*
+@tools\nant\NAnt.exe -targetframework:net-3.5 -buildfile:ExcelMapper.build -D:use-svn-revision=false -D:debug=true full
 PAUSE
